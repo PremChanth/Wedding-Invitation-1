6b61b329b42e25f6365f82e475bf5c16ca5665b7
PremChanth <premchanth.g@gmail.com>
2021-10-17 14:41:32 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -20,13 +20,15 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-$(document).on('click', function(){
-    document.getElementById("my_audio").play();
-    console.log('Shaadi me zaroor aana');
+$(document).on('mousemove', function(){
+    // setTimeout(()=> {
+        document.getElementById("my_audio").play();
+
+    // }, 2000)
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Nov 15, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -85,11 +87,4 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
-
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
-
-console.log(
-    `%cShaadi me zaroor aana!\n\n`,
-    'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
-)
+console.log('\n\n%c SAVE THE DATE: 14th Nov, 2021!', styles);
